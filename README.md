f4efa22cde74d6b39ce8217adece731bf621fe57
nakamotoshi <ioannisavgouleas@gmail.com>
2025-02-22 12:19:36 +0100

    Update instructions

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # react-fastapi-movie-recommender
-From the main project folder, to run the FastAPI backend:
+From the main project folder, run the FastAPI backend:
 
-> source backend/venv/bin/activate
-> python3 main.py
+``` 
+source backend/venv/bin/activate
+python3 main.py
+```
 
 To run the frontend (node.js, react and axios installed):
-> npm run dev
-
+```
+npm run dev
+```
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext.jsx';
+import { AuthProvider  } from './contexts/AuthContext.jsx';
 import Register from './components/Register.jsx';
 import Login from './components/Login.jsx';
 import UserProfile from './components/UserProfile.jsx';
